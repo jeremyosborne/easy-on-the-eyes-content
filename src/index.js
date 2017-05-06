1b635ccee2ec1e5c8aa6c8825690a2f5fc7b3924
Jeremy Osborne <jeremywosborne@gmail.com>
2017-05-06 11:41:55 -0700

    simplify

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-var content = require('./content')
-var selectors = require('./selectors')
+/**
+ * The basic content data type.
+ */
 
-module.exports = {
-  genContent: content.genContent,
+const lodash = require('lodash')
 
-  error: selectors.error,
-  isError: selectors.isError,
-  errorMessage: selectors.errorMessage,
-  isFetching: selectors.isFetching,
-  urlBeingFetched: selectors.urlBeingFetched
+// Basic content data type.
+const contentTemplate = {
+  // If truthy there is an error.
+  error: {
+    // Errors are non-zero codes.
+    code: 0,
+    // Errors must produce a non-falsey message.
+    message: ''
+  },
+  // Content either being retrieved or previously retrieved.
+  content: {
+    // name of transformer used to modify the content.
+    transformer: '',
+    // url content was retrieved from.
+    url: '',
+    // Type of the content: html, markdown, plain, etc.
+    type: '',
+    // the marked up text of the content.
+    text: '',
+  },
 }
+
+/**
+ * Returns an instance of the contentTemplate.
+ *
+ * @param {object} overrides Overrides for content. Assumes correct format.
+ * @return {object} Basic content object.
+ */
+const content = function (overrides) {
+  return lodash.merge({}, contentTemplate, overrides || {})
+}
+module.exports.content = content
--- src/index.test.js
+++ src/index.test.js
@@ -5,8 +5,20 @@
 var assert = require('assert')
 var mod = require('./index')
 
-describe('module smell test', function () {
-  it('exists', function () {
-    assert(mod)
+describe('content()', function () {
+  it('returns an object', function () {
+    assert(mod.content)
+  })
+
+  it('allows overrides', function () {
+    const overrides = {
+      content: {
+        text: 'hi'
+      }
+    }
+    const c = mod.content(overrides)
+    assert(c.content.text === 'hi')
+    // Basic structure remains.
+    assert('type' in c.content)
   })
 })
