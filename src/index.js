8d34a6242a2e8b688d03733fb22e3a60a00c3150
Jeremy Osborne <jeremywosborne@gmail.com>
2017-07-30 12:50:35 -0700

    Add wrapper.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const contentTemplate = {
 }
 
 /**
- * Returns an instance of the contentTemplate.
+ * Returns an instance of content.
  *
  * @param {object} overrides Overrides for content. Assumes correct format.
  * @return {object} Basic content object.
@@ -36,6 +36,69 @@ const create = function (overrides) {
   return _.merge({}, contentTemplate, overrides || {})
 }
 
+/**
+ * Content wrapper class.
+ *
+ * @param {Object} content object to wrap and provide content helpers to.
+ * @return {Object} implementation of content helpers/interface.
+ */
+const Content = function (content) {
+  this.content = content || {}
+}
+/**
+ * Is there a known error with this content?
+ *
+ * @return {String} error message, or empty string if no error message.
+ */
+Content.prototype.errorMessage = function () {
+  return _.get(this.content, 'error.message', '')
+}
+/**
+ * Is there a known error with this content?
+ *
+ * @return {Boolean} true if error, false if not.
+ */
+Content.prototype.isError = function () {
+  return !!_.get(this.content, 'error.code', false)
+}
+/**
+ * The main text content of this content object.
+ *
+ * If markup, this will return all the markup along with the 'text'.
+ *
+ * @type {String} the main content of the cotnent object.
+ */
+Content.prototype.text = function () {
+  return _.get(this.content, 'content.text', '')
+}
+/**
+ * The media type of the main content.
+ *
+ * @type {String} media type enumeration (as string) or empty string if unknown.
+ */
+Content.prototype.type = function () {
+  return _.get(this.content, 'content.type', '')
+}
+/**
+ * The location the main portion of the content came from.
+ *
+ * @type {String} url or empty string.
+ */
+Content.prototype.url = function () {
+  return _.get(this.content, 'content.url', '')
+}
+
+/**
+ * Wraps an object and provides safe helper methods.
+ *
+ * @param {Object} content presumed content object.
+ * @return {Object} wrapped object.
+ */
+const content = function (content) {
+  return new Content(content)
+}
+
 module.exports = {
+  content,
   create,
 }
--- src/index.test.js
+++ src/index.test.js
@@ -6,6 +6,29 @@ var assert = require('assert')
 var content = require('./index')
 
 describe('content', function () {
+  describe('content()', function () {
+    it('it works', function () {
+      const data = {
+        error: {
+          code: 1,
+          message: 'some helpful error message'
+        },
+        content: {
+          transformer: 'wikipedia',
+          url: 'https://wikipedia.org',
+          type: 'text/html',
+          text: '<h1>Some headline</h1>',
+        },
+      }
+      const c = content.content(data)
+      assert(c.errorMessage() === data.error.message)
+      assert(c.isError() === true)
+      assert(c.text() === data.content.text)
+      assert(c.type() === data.content.type)
+      assert(c.url() === data.content.url)
+    })
+  })
+
   describe('create()', function () {
     it('allows overrides', function () {
       const overrides = {
